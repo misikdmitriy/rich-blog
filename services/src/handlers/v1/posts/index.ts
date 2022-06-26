8793eb0e01f3e4cb970001722faaabd448fcfe72
d.misik <misik.dmitriy@gmail.com>
2022-06-26 21:23:07 +0300

    update BE

--- services/src/handlers/v1/posts/index.ts
+++ services/src/handlers/v1/posts/index.ts
@@ -1,9 +1,25 @@
+import { Express } from 'express';
+import { body, param, query } from 'express-validator';
 import create from './create';
 import getById from './getById';
 import get from './get';
 
-export default {
-	create,
-	getById,
-	get,
+const postsV1 = (app: Express) => {
+	app.get(
+		'/v1/posts',
+		query('pageNum').isInt({ min: 0 }).optional(),
+		query('pageSize').isInt({ min: 1, max: 500 }).optional(),
+		get,
+	);
+	app.get('/v1/posts/:id', param('id').notEmpty(), getById);
+
+	app.put(
+		'/v1/posts',
+		body('name').isLength({ max: 500 }).trim().escape()
+			.notEmpty(),
+		body('body').notEmpty(),
+		create,
+	);
 };
+
+export default postsV1;
--- services/src/index.ts
+++ services/src/index.ts
@@ -1,28 +1,12 @@
 import './config-enrich';
 import express from 'express';
-import { body, param, query } from 'express-validator';
-import PostsV1 from './handlers/v1/posts';
+import postsV1 from './handlers/v1/posts';
 
 const app = express();
 app.use(express.json());
+postsV1(app);
 
 const port = Number(process.env.PORT || 3000);
-
-app.get(
-	'/v1/posts',
-	query('pageNum').isInt({ min: 0 }).optional(),
-	query('pageSize').isInt({ min: 1, max: 500 }).optional(),
-	PostsV1.get,
-);
-app.get('/v1/posts/:id', param('id').notEmpty(), PostsV1.getById);
-
-app.put(
-	'/v1/posts',
-	body('name').isLength({ max: 500 }).trim().escape()
-		.notEmpty(),
-	body('body').notEmpty(),
-	PostsV1.create,
-);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
