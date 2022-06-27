8f5a74a007054ab9165d336268337207c20fd6de
Dmytro Misik <d.misik@draftkings.com>
2022-06-27 15:13:08 +0300

    place login in center

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,6 +1,18 @@
+import { Grid } from '@mui/material';
 import React from 'react';
 import LoginWithGoogle from './LoginWithGoogle';
 
-const Login = () => <LoginWithGoogle />;
+const Login = () => (
+	<Grid
+		container
+		spacing={0}
+		direction="column"
+		alignItems="center"
+		justifyContent="center"
+		style={{ minHeight: '100vh' }}
+	>
+		<Grid item xs={6}><LoginWithGoogle /></Grid>
+	</Grid>
+);
 
 export default Login;
