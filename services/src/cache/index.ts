f24fc643da5f86e640297c96fa592d46d3112e0f
d.misik <misik.dmitriy@gmail.com>
2022-07-03 21:43:47 +0300

    more tests

--- services/src/cache/index.ts
+++ services/src/cache/index.ts
@@ -5,7 +5,7 @@ const cache = new NodeCache();
 export const getOrSet = <T = unknown>(
 	key: string,
 	itemFactory: () => T,
-	ttl: number, // 15 mins
+	ttl: number = 15 * 60, // 15 mins
 ): T => {
 	const item = cache.get(key);
 	if (item !== undefined) {
@@ -20,7 +20,7 @@ export const getOrSet = <T = unknown>(
 export const getOrSetAsync = async <T = unknown>(
 	key: string,
 	itemFactory: () => Promise<T>,
-	ttl: number, // 15 mins
+	ttl: number = 15 * 60, // 15 mins
 ): Promise<T> => {
 	const item = cache.get(key);
 	if (item !== undefined) {
