29552e9b2f5c0cb8fbd823b715678656a04b8065
Dmytro Misik <d.misik@draftkings.com>
2022-07-07 12:26:26 +0300

    create page

--- frontend/src/components/createPost/CreatePostPage.tsx
+++ frontend/src/components/createPost/CreatePostPage.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import React, { useEffect } from 'react';
+import {
+	Formik, Form, Field, useFormikContext, FormikContextType,
+} from 'formik';
 import * as Yup from 'yup';
 import { Box, TextField, Button } from '@mui/material';
 
@@ -9,61 +11,134 @@ const schema = Yup.object().shape({
 	description: Yup.string().required(),
 });
 
-const CreatePostPage = () => (
-	<Box sx={{ display: 'flex' }} alignItems="center" justifyContent="center">
-		<Formik
-			initialValues={{
-				title: '',
-				shortUrl: '',
-				description: '',
-			}}
-			validationSchema={schema}
-			onSubmit={() => {}}
-		>
-			{() => (
-				<Form style={{ display: 'flex', flexDirection: 'column' }}>
-					<Field
-						name="title"
-						component={TextField}
-						margin="normal"
-						required
-						label="Title"
-						type="text"
-						id="title"
-					/>
-					<Field
-						name="shortUrl"
-						component={TextField}
-						margin="normal"
-						required
-						label="Short URL"
-						type="text"
-						id="title"
-					/>
-					<Field
-						name="description"
-						component={TextField}
-						margin="normal"
-						multiline
-						rows={4}
-						required
-						label="Description"
-						type="text"
-						id="title"
-					/>
-					<Button
-						sx={{ m: 1 }}
-						variant="outlined"
-						size="large"
-						color="success"
-					>
-						Create
+const generateShortUrl = (title: string) => title.trim().replace(/[\s.,:?]+/g, '-').toLowerCase();
 
-					</Button>
-				</Form>
-			)}
-		</Formik>
-	</Box>
-);
+interface CreatePostValues {
+	title: string,
+	shortUrl: string,
+	description: string,
+}
+
+const CreatePostContext: React.FC = () => {
+	const {
+		values: { title },
+		setFieldValue,
+	} = useFormikContext<CreatePostValues>();
+
+	useEffect(() => {
+		setFieldValue('shortUrl', generateShortUrl(title));
+	}, [title]);
+
+	return null;
+};
+
+const CreatePostPage = () => {
+	function createTextField <TName extends keyof CreatePostValues & string>(
+		name: TName,
+		title: string,
+		{
+			values: {
+				[name]: value,
+			},
+			setFieldValue,
+			touched: {
+				[name]: touched,
+			},
+			errors: {
+				[name]: errors,
+			},
+		}: FormikContextType<CreatePostValues>,
+		other: Record<string, unknown> = {},
+	) {
+		return (
+			<Field
+				name={name}
+				fullWidth
+				component={TextField}
+				margin="normal"
+				label={title}
+				value={value}
+				error={touched && Boolean(errors)}
+				helperText={touched && errors}
+				type="text"
+				id={name}
+				onChange={
+					(event: React.ChangeEvent<HTMLInputElement>) => setFieldValue(name, event.target.value)
+				}
+				{...other}
+			/>
+		);
+	}
+
+	return (
+		<Box
+			sx={{ display: 'flex' }}
+			alignItems="center"
+			justifyContent="center"
+		>
+			<Box
+				sx={{
+					display: 'flex',
+					width: {
+						sx: '90vw',
+						md: '60vw',
+					},
+				}}
+				alignItems="center"
+				justifyContent="center"
+			>
+				<Formik
+					initialValues={{
+						title: '',
+						shortUrl: '',
+						description: '',
+					}}
+					validationSchema={schema}
+					onSubmit={() => { }}
+				>
+					{(values: FormikContextType<CreatePostValues>) => (
+						<Form style={{
+							display: 'flex',
+							flexDirection: 'column',
+							flexGrow: 1,
+						}}
+						>
+							{createTextField('title', 'Title', values)}
+							<Box sx={{
+								display: 'flex',
+								flexDirection: 'row',
+							}}
+							>
+								{createTextField('shortUrl', 'Short URL', values)}
+								<Button
+									sx={{ m: 3 }}
+									size="small"
+									color="secondary"
+									onClick={() => {
+										values.setFieldValue('shortUrl', generateShortUrl(values.values.title));
+									}}
+								>
+									Regenerate
+								</Button>
+							</Box>
+							{createTextField('description', 'Description', values, { multiline: true, rows: 3 })}
+							<Button
+								sx={{ m: 1 }}
+								variant="outlined"
+								size="large"
+								color="success"
+								type="submit"
+							>
+								Create
+
+							</Button>
+							<CreatePostContext />
+						</Form>
+					)}
+				</Formik>
+			</Box>
+		</Box>
+	);
+};
 
 export default CreatePostPage;
